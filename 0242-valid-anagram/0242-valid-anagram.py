74a4701aa25395e597973fc2a8e919e7bb792a21
Arnav0605 <rawatarnav05@gmail.com>
2026-08-30 23:32:00 +0530

    Time: 12 ms (55.76%), Space: 19.4 MB (45.98%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        hash= [0] * 26
+        for ch in s:
+            index = ord(ch) - ord('a')
+            hash[index] +=1
+        for ch in t:
+            index = ord(ch) - ord('a')
+            hash[index] -=1
+
+        return hash == [0] *26
+        if hash==0:
+            return True
+        else:
+            return False
